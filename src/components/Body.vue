2104459a5af1700e634c633decc94b70a89caa2e
Edozie <phynnedo@gmail.com>
2021-08-28 21:23:51 +0100

    Initial Commit

---
+++ src/components/Body.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="container">
+        <input type="number" v-model.number="num1">
+        <span>+</span>
+        <input type="number" v-model.number="num2">
+        <span>=</span>
+        <span>{{num1 + num2}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            num1: 0,
+            num2: 0
+        }
+    }
+}
+</script>
+
+<style scoped>
+    #container{
+        background-color: #42b983;
+        height: 300px;
+        width: 600px;
+        padding-top: 20px;
+        margin: 0 auto;
+        border-radius: 10px;
+    }
+
+    input, span{
+        padding: 10px;
+        font-size: 30px;
+        font-family: 'Work Sans', sans-serif;
+    }
+
+    input{
+        width: 40px;
+        text-align: center;
+    }
+
+    span:last-of-type{
+        background-color: #9bfcd0;
+        width: 60px;
+        border-radius: 5px;
+    }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>{{text}}</h2>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data(){
+    return{
+      text: 'A Simple Reactive Calculator created with Vue.'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h2 {
+  margin: 40px 0 20px 0;
+}
+
+a {
+  color: #42b983;
+}
+</style>
